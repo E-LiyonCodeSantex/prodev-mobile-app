69e93d0f112236d0f9f1de41794369f3008feb7a
E-LiyonCodeSantex <eelyondesign@gmail.com>
2025-09-07 19:36:19 +0100

    1. Navigation using Tabs Navigation Patterns

---
+++ prodev-mobile-app-0x05/app/(home)/profile.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+const profile = () =>{
+    return (
+        <View>
+            <Text> Profile Screen</Text>
+        </View>
+    )
+}
---
+++ prodev-mobile-app-0x05/app/(home)/saved.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+const profile = () =>{
+    return (
+        <View>
+            <Text> Save Screen</Text>
+        </View>
+    )
+}
